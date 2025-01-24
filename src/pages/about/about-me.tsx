b1892e7ec3a1e0301b7ecc8e0478c327523790cb
fullstack developer <fullstackdeveloper918@gmail.com>
2025-01-24 13:15:17 +0530

    error resolve

--- src/pages/about/about-me.tsx
+++ src/pages/about/about-me.tsx
@@ -15,10 +15,15 @@ import AboutPortfolio from "@/components/portfolio/about-portfolio";
 import AboutWork from "@/components/work/about-work";
 import { BrandItems } from "@/components/brand/brand-two";
 import LineImgSlider from "@/components/line-text/line-img-slider";
-import BigText from "@/components/big-text";
+// import BigText from "@/components/big-text";
+// import BigText from "@/components/big-text";
 import FooterTwo from "@/layouts/footers/footer-two";
 // animation
-import {charAnimation,fadeAnimation,titleAnimation} from "@/utils/title-animation";
+import {
+  charAnimation,
+  fadeAnimation,
+  titleAnimation,
+} from "@/utils/title-animation";
 import { hoverBtn } from "@/utils/hover-btn";
 import { teamMarqueAnim } from "@/utils/scroll-marque";
 
@@ -38,30 +43,19 @@ const AboutMeMain = () => {
 
   return (
     <Wrapper>
-      {/* header area start */}
       <HeaderEleven />
-      {/* header area end */}
 
       <div id="smooth-wrapper">
         <div id="smooth-content">
           <main>
-            {/* about hero */}
             <AboutMeHero />
-            {/* about hero */}
 
-            {/* about me area */}
             <AboutMeArea />
-            {/* about me area */}
 
-            {/* about portfolio */}
             <AboutPortfolio />
-            {/* about portfolio */}
 
-            {/* about work */}
             <AboutWork />
-            {/* about work */}
 
-            {/* brand area */}
             <div className="tp-brand-4-area pt-120 pb-120">
               <div className="container">
                 <div className="row gx-0">
@@ -69,20 +63,13 @@ const AboutMeMain = () => {
                 </div>
               </div>
             </div>
-            {/* brand area */}
 
-            {/* line img slider */}
-            <LineImgSlider/>
-            {/* line img slider */}
+            <LineImgSlider />
 
-            {/* big text */}
-            <BigText cls="char-wrapper" />
-            {/* big text */}
+            {/* <BigText whyChooseData  cls="char-wrapper" /> */}
           </main>
 
-          {/* footer area */}
           <FooterTwo topCls="" />
-          {/* footer area */}
         </div>
       </div>
     </Wrapper>
