1700bb74cc3d50c7e85027aad98bb67ac4168706
fullstack developer <fullstackdeveloper918@gmail.com>
2025-01-24 13:27:12 +0530

    bvd

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen w-full">
       <div role="status">
         <svg
           aria-hidden="true"
